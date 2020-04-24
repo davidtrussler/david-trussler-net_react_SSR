d34a0285c1d73889189cc7d2da02c363c577ef82
David Trussler <mail@davidtrussler.net>
2020-04-24 08:13:08 +0100

    Adds event listener

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const App = () => {
+	function handleEvent(e) {
+		e.preventDefault(); 
+		console.log('I was clicked!'); 
+	}
+
   return (
 	  <div>
 	    <h1>Hello World, React!</h1>
-	    <h2>You are great!!</h2>
+	    <h2>You are great!</h2>
+	    <a href="/?event=click" onClick={handleEvent}>Click me!</a>
 	  </div>
   )
 }
