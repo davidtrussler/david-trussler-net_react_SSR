12eb6204b3a16b28590a86be87b969b01169034d
David Trussler <mail@davidtrussler.net>
2020-04-25 18:55:29 +0100

    Creates new Music component

---
+++ src/shared/Music.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Header} from '../components/Header'; 
+import {Footer} from '../components/Footer'; 
+
+const Music = () => {
+  return (
+    <React.Fragment>
+      <Header/>
+      <main>
+      	<p>Content for the music page goes here</p>
+      </main>
+      <Footer/>
+    </React.Fragment>
+  )
+}
+
+export default Music
