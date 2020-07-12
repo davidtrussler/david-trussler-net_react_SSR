847929c6d69c27bf0f782096a17778aba9792527
David Trussler <mail@davidtrussler.net>
2020-07-12 22:16:48 +0100

    Adds DATABASE_URL to connectionString

--- src/db/index.js
+++ src/db/index.js
@@ -7,11 +7,11 @@ const { Client } = require('pg');
 // const port = '5432'; 
 // const connectionString = 'postgres://' + user + ':' + password + '@' + host + ':' + port + '/' + database; 
 // const connectionString = 'postgres://dbfxyrdtlddylg:9bfedda067aea7c8d2fa3bf1608665ffb274454b9297afdc73ec9887cf69718e@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/d5telim30j1g04'; 
-const connectionString = process.env.DATABASE_URL; 
+// const connectionString = process.env.DATABASE_URL; 
 // const connectionString = 'postgres://localhost:5432/postgresql-colorful-81881'; 
 // const connectionString = 'blog'; 
 let client = new Client({
-	connectionString: connectionString
+	connectionString: process.env.DATABASE_URL
 	// ssl: {
 	// 	rejectUnauthorized: false
 	// }
