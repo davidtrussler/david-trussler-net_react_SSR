42c09cf219cbc1f3cd1e25c57d6d83e5873094f8
David Trussler <mail@davidtrussler.net>
2020-07-13 18:35:23 +0100

    Installs dotenv module and adds .env file to store local db connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,20 +1,9 @@
-const { Client } = require('pg'); 
+require('dotenv').config(); 
 
-// const user = 'davidtrussler';
-// const host = 'localhost';
-// const database = 'postgresql-colorful-81881';
-// const password = 'root'; 
-// const port = '5432'; 
-// const connectionString = 'postgres://' + user + ':' + password + '@' + host + ':' + port + '/' + database; 
-// const connectionString = 'postgres://dbfxyrdtlddylg:9bfedda067aea7c8d2fa3bf1608665ffb274454b9297afdc73ec9887cf69718e@ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/d5telim30j1g04'; 
-// const connectionString = process.env.DATABASE_URL; 
-// const connectionString = 'postgres://localhost:5432/postgresql-colorful-81881'; 
-// const connectionString = 'blog'; 
+const {Client} = require('pg'); 
+
 let client = new Client({
-	connectionString: 'postgres://localhost:5432/postgresql-colorful-81881' // process.env.DATABASE_URL
-	// ssl: {
-	// 	rejectUnauthorized: false
-	// }
+	connectionString: process.env.DATABASE_URL
 }); 
 
 client.connect(); 
