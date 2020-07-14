d2b65e105f55ed6f99d0f6d591fd053b91e20329
David Trussler <mail@davidtrussler.net>
2020-07-14 08:13:41 +0100

    Brings call to getPosts() into page load (WIP)

--- src/components/blog/Main.js
+++ src/components/blog/Main.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-let db = require('../../db'); 
-let rows = db.getPosts(); 
+const db = require('../../db'); 
+
+let rows = []; 
 
 export function Main() {
+	var results = new Promise(function(resolve, reject) {
+		// setTimeout(function() {
+			// resolve(myfunction); 
+		// }, 2000); 
+		resolve(db.getPosts()); 
+	}); 
+
+	results.then(function(msg) {
+		console.log(msg); 
+	})
+
+	// db.getPosts().then(rows => {
+	// 	console.log('rows: ', rows)
+	// }).catch(error => {
+	// 	console.log('error: ', error)
+	// }); 
+
+	// let result = db.getPosts();
+
+	// console.log('result: ', db.getPosts()); 
+
+	// db.getPosts(rows => {
+	// 	// console.log('rows: ', rows); 
+	// }); 
+
 	return (
 		<main>
 			<div className="constrained">
--- src/db/index.js
+++ src/db/index.js
@@ -2,23 +2,47 @@ require('dotenv').config();
 
 const {Client} = require('pg'); 
 
-let client = new Client({
-	connectionString: process.env.DATABASE_URL
-}); 
+// const client = new Client({
+// 	connectionString: process.env.DATABASE_URL
+// }); 
 
-client.connect(); 
+// client.connect(); 
 
-let rows = []; 
-let getPosts = () => {
+const getPosts = () => {
+	console.log('getPosts!'); 
+
+	let client = new Client({
+		connectionString: process.env.DATABASE_URL
+	}); 
+
+	// console.log('client: ', client); 
+
+	client.connect(); 
+
+	let rows = []; 
+
 	client.query('SELECT * FROM posts;', (err, res) => {
 		if (err) throw err;
 
-		for (let row of res.rows) {
+		// console.log('result: ', res.rows); 
+
+		res.rows.forEach(function(row) {
+			// console.log('row: ', row); 
 			rows.push(row); 
-		}
+		}); 
+
+		// for (let row of res.rows) {
+		// 	rows.push(row); 
+		// }
 
 		client.end();
+
+		// console.log('rows: ', rows); 
+
+	  return rows; 
 	});
+
+	// return 'hello'; 
 
   return rows; 
 }
