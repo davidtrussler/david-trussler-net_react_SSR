b1e3ee4d147638ec57bae7d05526cedb5be601c2
David Trussler <mail@davidtrussler.net>
2020-07-17 17:37:18 +0100

    Sets up methods to handle server requests - handleRender - renderFullPage

--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,51 @@
 import React from 'react'
 import express from 'express'
+import { createStore } from 'redux'; 
+import { Provider } from 'react-redux'; 
 import ReactDOMServer from 'react-dom/server';
 import App from '../shared/App.js'
 import scss from '../shared/App.scss'; 
-import {StaticRouter} from 'react-router'; 
+import { StaticRouter } from 'react-router'; 
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express(); 
+const port = process.env.PORT || 4000; 
 
-app.use(express.static('public'))
+// Seve static files
+app.use(express.static('public')); 
 
-app.get('*', (req, res) => {
-  const markup = ReactDOMServer.renderToString(
-  	<StaticRouter location={req.url}>
-	  	<App/>
-  	</StaticRouter>
-	)
+// Fired every time the server receives a request
+app.use(handleRender); 
 
-	res.send(`
-		<!doctype html>
-		<html>
-			<head>
-				<title>David Trussler</title>
-				<meta charset="utf-8"/>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<style>${scss}</style>
-			</head>
-			<body>
-			  <div id="app">${markup}</div>
-			</body>
-		</html>
-	`)
-})
+function handleRender(req, res) {
+	console.log('handleRender!'); 
+}; 
+
+function renderFullPage(html, preLoadedState) {
+	console.log('renderFullPage')
+}; 
+
+// app.get('*', (req, res) => {
+//   const markup = ReactDOMServer.renderToString(
+//   	<StaticRouter location={req.url}>
+// 	  	<App/>
+//   	</StaticRouter>
+// 	)
+
+// 	res.send(`
+// 		<!doctype html>
+// 		<html>
+// 			<head>
+// 				<title>David Trussler</title>
+// 				<meta charset="utf-8"/>
+// 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 				<style>${scss}</style>
+// 			</head>
+// 			<body>
+// 			  <div id="app">${markup}</div>
+// 			</body>
+// 		</html>
+// 	`)
+// })
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`)
