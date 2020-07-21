999033c99407b521cec882dd2bc0ccdcefd50430
David Trussler <mail@davidtrussler.net>
2020-07-21 23:42:06 +0100

    Shifts store stuff from server to blog component

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Header} from '../shared/Header'; 
-import {Main} from './Main'; 
-import {Footer} from '../shared/Footer'; 
+import { createStore } from 'redux'; 
+import { Provider } from 'react-redux';
+import { modelReducer } from '../../store/modelReducer'; 
+import { Header } from '../shared/Header'; 
+import { Main } from './Main'; 
+import { Footer } from '../shared/Footer'; 
 
 const Blog = () => {
+	// Creates new Redux store instance
+	const store = createStore(modelReducer); 
+
+	console.log('store: ', store); 
+
   return (
-    <React.Fragment>
-      <Header/>
-      <Main/>
-      <Footer/>
-    </React.Fragment>
+  	<Provider store={store}>
+	    <React.Fragment>
+	      <Header/>
+	      <Main/>
+	      <Footer/>
+	    </React.Fragment>
+    </Provider>
   )
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import express from 'express'
-import { createStore } from 'redux'; 
-// import configureStore from '../store/configureStore'
-import { Provider } from 'react-redux';
-// import { createStore } from '../store'; 
-import {modelReducer} from '../store/modelReducer'; 
 import ReactDOMServer from 'react-dom/server';
 import App from '../shared/App.js'
 import scss from '../shared/App.scss'; 
 import { StaticRouter } from 'react-router'; 
 
-// const store = configureStore(); 
 const app = express(); 
 const port = process.env.PORT || 4000; 
 
@@ -23,16 +17,11 @@ app.use(handleRender);
 function handleRender(req, res) {
 	console.log('handleRender!'); 
 
-	// Creates new Redux store instance
-	const store = createStore(modelReducer); 
-
 	// Render component to string
   const markup = ReactDOMServer.renderToString(
-  	<Provider store={store}>
-	  	<StaticRouter location={req.url}>
-		  	<App/>
-	  	</StaticRouter>
-  	</Provider>
+  	<StaticRouter location={req.url}>
+	  	<App/>
+  	</StaticRouter>
 	)
 
 	res.send(renderFullPage(markup)); 
@@ -56,29 +45,6 @@ function renderFullPage(markup) {
 		</html>
 	`
 }; 
-
-// app.get('*', (req, res) => {
-//   const markup = ReactDOMServer.renderToString(
-//   	<StaticRouter location={req.url}>
-// 	  	<App/>
-//   	</StaticRouter>
-// 	)
-
-// 	res.send(`
-// 		<!doctype html>
-// 		<html>
-// 			<head>
-// 				<title>David Trussler</title>
-// 				<meta charset="utf-8"/>
-// 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-// 				<style>${scss}</style>
-// 			</head>
-// 			<body>
-// 			  <div id="app">${markup}</div>
-// 			</body>
-// 		</html>
-// 	`)
-// })
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`)
