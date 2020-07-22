4eebe3877e9c21e41b5482d093a0ca5b9ce0b69f
David Trussler <mail@davidtrussler.net>
2020-07-22 21:35:59 +0100

    Fetches data fro the blog page from Initial Data

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { createStore } from 'redux'; 
-import { Provider } from 'react-redux';
-import { modelReducer } from '../../store/modelReducer'; 
 import { Header } from '../shared/Header'; 
 import { Main } from './Main'; 
 import { Footer } from '../shared/Footer'; 
+import { Provider } from 'react-redux'; 
+import dataStore from '../../store'; 
 
 const Blog = () => {
-	// Creates new Redux store instance
-	const store = createStore(modelReducer); 
-
-	console.log('store: ', store); 
-
   return (
-  	<Provider store={store}>
+  	<Provider store={dataStore}>
 	    <React.Fragment>
 	      <Header/>
-	      <Main/>
+	      <Main posts={dataStore.getState().posts}/>
 	      <Footer/>
 	    </React.Fragment>
     </Provider>
--- src/components/blog/Main.js
+++ src/components/blog/Main.js
@@ -1,43 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const db = require('../../db'); 
+export function Main(posts) {
+	return (
+		<main>
+			<div className="constrained">
+				<div className="main__inner">
+					<p>Content for the main section of the blog landing page goes here.</p>
 
-export class Main extends Component {
-	constructor() {
-		super(); 
-
-		this.state = {
-			rows: []
-		}
-	}
-
-	render() {
-		let _this = this;
-
-		db.getPosts().then(function(response) {
-			response.forEach(function(row) {
-				console.log('row: ', row); 
-
-				_this.setState({
-					rows: response
-				}); 
-			}); 
-		})
-
-		return (
-			<main>
-				<div className="constrained">
-					<div className="main__inner">
-						<p>Content for the main section of the blog landing page goes here.</p>
-
-						<ul>
-							{this.state.rows.map(
-								(post, i) => <li key={i}>{post.date}: {post.title}</li>
-							)}
-						</ul>
-					</div>
+					<ul>
+						{posts.posts.map((post, i) => <li key={i}>{post.date}: {post.title}</li>)}
+					</ul>
 				</div>
-			</main>
-		)
-	}
+			</div>
+		</main>
+	)
 }
